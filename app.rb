bd4005f92a89cc48627fdcc309f111c3a01349b9
Brown Bathrobe <brownbathrobe@hotmail.com>
2012-08-21 15:46:32 -0400

    add current time varible to app.rb

--- app.rb
+++ app.rb
@@ -2,5 +2,15 @@ require "sinatra"
 require "shotgun"
 
 get "/" do
-  File.read('index.html')
+  time = Time.now
+  @hours = time.hour  > 11 ? time.hour - 12 : time.hour
+  @mins = time.min
+  @secs = time.sec
+
+  @hours = 100
+  @mins = 200
+  @secs = 300
+  puts "#{@hours}:#{@mins}:#{@secs}"
+  erb :index
+  # File.read('index.html')
 end
