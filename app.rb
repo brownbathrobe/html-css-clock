97666364d73e36bf3cfeb8991ceb166932a66140
Dave Maurakis <dave@indusblue.com>
2012-08-21 20:12:08 -0400

    add additional styles, use index.html as a template

--- app.rb
+++ app.rb
@@ -2,23 +2,23 @@ require "sinatra"
 require "shotgun"
 
 get "/" do
-  time   = Time.now
-  secs   = time.sec
-  mins   = time.min
-  hours  = time.hour > 11 ? time.hour - 12 : time.hour
+  time = Time.now
+  secs = time.sec
+  mins = time.min
+  hours = time.hour > 11 ? time.hour - 12 : time.hour
 
   mins_in_secs = mins * 60
-  hrs_in_secs  = hours * 3600
+  hrs_in_secs = hours * 3600
 
   degrees = 360
   secs_in_1_min = 60.0
   secs_in_1_hr = 3600.0
   secs_in_12_hrs = 43200.0
 
-  @secs  = secs / secs_in_1_min * degrees
-  @mins  = (mins_in_secs + secs) / secs_in_1_hr * degrees
+  @secs = secs / secs_in_1_min * degrees
+  @mins = (mins_in_secs + secs) / secs_in_1_hr * degrees
   @hours = (hrs_in_secs + mins_in_secs + secs) / secs_in_12_hrs * degrees
 
-  puts "#{@hours}:#{@mins}:#{@secs}"
-  erb :index
+  styles = [:roman, :numeric, :dashes, :silly]
+  erb styles.sample, :layout => :index
 end
